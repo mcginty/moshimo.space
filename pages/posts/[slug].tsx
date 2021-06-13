30ab9dcc43d0a1363f44c6135950ce62029041f0
Jake McGinty <me@jake.su>
2021-06-13 22:17:11 +0900

    fix build again

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,16 +1,11 @@
 import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
 import Container from '../../components/container'
-import PostBody from '../../components/post-body'
 import Header from '../../components/header'
 import PostHeader from '../../components/post-header'
 import Layout from '../../components/layout'
-import { getEvents } from '../../lib/api'
 import PostTitle from '../../components/post-title'
 import Head from 'next/head'
-import { CMS_NAME } from '../../lib/constants'
-import markdownToHtml from '../../lib/markdownToHtml'
-import Event from '../../types/event'
+import { Event } from '../../types/event'
 
 type Props = {
   event: Event
